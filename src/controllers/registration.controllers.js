b7d397d1f75287a0760701e0444a83778be699e4
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-11-05 22:23:10 +0300

    activate user

--- src/controllers/registration.controllers.js
+++ src/controllers/registration.controllers.js
@@ -12,7 +12,13 @@ class RegistrationControllers {
         }
     }
     async activate(req, res, next) {
-
+        try {
+            const activatedLink = req.params.link
+            await RegistrationService.activate(activatedLink)
+            return res.json('Аккаунт активирован')
+        } catch (error) {
+            next(error)
+        }
     }
 }
 
