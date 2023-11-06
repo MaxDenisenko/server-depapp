12b9a67e93e505575e9ea183cdaabb7363d6e66d
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-11-06 11:54:58 +0300

    add res.cookie

--- src/controllers/registration.controllers.js
+++ src/controllers/registration.controllers.js
@@ -5,8 +5,8 @@ class RegistrationControllers {
         try {
             const {name, lastname, email, password} = req.body
             const userData = await RegistrationService.registration(name, lastname, email, password)
+            res.cookie('refreshToken',userData.refreshToken,{maxAge: 30*24*60*60*1000, httpOnly: true, secure: true})
             return res.json(userData)
-            
         } catch (error) {
             next(error)
         }
