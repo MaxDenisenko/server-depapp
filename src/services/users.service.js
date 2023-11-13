55b521174688826fbd0d867681b9da0cc1fcd7d6
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-11-13 22:56:15 +0300

    fix getUser

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -3,7 +3,7 @@ const usersModels = require('../models/users.models')
 class UsersService {
     async getUser() {
         try {
-            const user = await usersModels.findOne()
+            const user = await usersModels.findAll()
             return user
         } catch (error) {
                 console.log(error)
